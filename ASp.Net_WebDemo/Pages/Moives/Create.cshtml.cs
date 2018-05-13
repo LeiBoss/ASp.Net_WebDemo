4746cb0232ec5ad8d5731441a036a2363fa15a51
LeiBoss <1902862824@qq.com>
2018-05-13 23:47:20 +0800

    1.添加Model

---
+++ ASp.Net_WebDemo/Pages/Moives/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ASp.Net_WebDemo.Models;
+
+namespace ASp.Net_WebDemo.Pages.Moives
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ASp.Net_WebDemo.Models.MovieContext _context;
+
+        public CreateModel(ASp.Net_WebDemo.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ASp.Net_WebDemo/Pages/Moives/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ASp.Net_WebDemo.Models;
+
+namespace ASp.Net_WebDemo.Pages.Moives
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ASp.Net_WebDemo.Models.MovieContext _context;
+
+        public IndexModel(ASp.Net_WebDemo.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
